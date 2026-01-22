d5da1dc295ce4158cbae86979f215ebbaf1e7a6e
Junior-Osborn <juniorkhatib7@gmail.com>
2026-01-22 06:02:49 +0300

    execution api

--- src/api/api.js
+++ src/api/api.js
@@ -18,6 +18,7 @@ export const serviceAPI = {
   getAll: () => api.get("/services"),
   create: (data) => api.post("/services", data),
   update: (id, data) => api.put(`/services/${id}`, data),
+  execute: (id, data) => api.post(`/services/${id}/execute`, data),
   delete: (id) => api.delete(`/services/${id}`),
 };
 
